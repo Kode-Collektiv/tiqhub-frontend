a27ee5289ce21f25e2aa0627974ebde12b58d80b
j-sattler <mail@sattlerjoshua.de>
2020-11-21 18:18:32 +0100

    Add go button

--- src/components/TickerStarter.vue
+++ src/components/TickerStarter.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="starter-container">
     <h1>Join or Create Ticker</h1>
-    <input id="ticker-starter" type="text" placeholder="Enter Ticker-ID...">
+    <input v-model="tickerId" id="ticker-starter" placeholder="Enter Ticker-ID...">
+    <button class="go-button" @click="go">GO!</button>
+    <h1>Ticker-ID: {{ tickerId }}</h1>
   </div>
 </template>
 
@@ -14,7 +16,11 @@ import {Options, Vue} from 'vue-class-component';
   }
 })
 export default class TickerCard extends Vue {
+  tickerId = '';
 
+  go() {
+    console.log(this.tickerId)
+  }
 }
 </script>
 
@@ -26,10 +32,16 @@ h1 {
 }
 
 .starter-container {
+  display: grid;
+  grid-template-columns: auto;
+  grid-gap: 1em;
   border: rgba(100, 100 , 100, 0.8) solid 0.1em;
   border-radius: 20px;
   box-shadow: 0 2px 4px 0 rgba(0,0,0,0.4);
   padding: 1em;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
 }
 
 input[type=text]{
@@ -41,6 +53,17 @@ input[type=text]{
   font-size: 1em;
   font-weight: bold;
   letter-spacing: 2px;
+}
+
+.go-button {
+  background-color: #2c3e50;
+  color: white;
+  border: rgba(255, 255 , 255, 1.0) solid 0.2em;
+  border-radius: 10px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  letter-spacing: 3px;
+  font-weight: bold;
 }
 
 </style>
