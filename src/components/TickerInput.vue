59d75d913a1d4ee7a48b1947b81051e30bf7c5e5
Nico Müller <nicomueller1991@googlemail.com>
2020-11-22 00:45:33 +0100

    Make feed button nice

--- src/components/TickerInput.vue
+++ src/components/TickerInput.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="field">
-    <div class="control">
-      <input class="input is-primary" v-model="message" type="text" placeholder="Primary input">
-      <button class="send-button" @click="go">Feed!</button>
+
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input is-primary" v-model="message" type="text">
+      </p>
+      <p class="control">
+        <a class="button is-link is-primary" @click="go">
+          FEED!
+        </a>
+      </p>
     </div>
   </div>
 
@@ -21,6 +28,7 @@ export default class TickerInput extends Vue {
     const uts = Math.round(Date.now() / 1000)
     const parent: any = this.$parent;
     const output = JSON.stringify({text: this.message, timestamp: uts})
+
     parent.socket.emit(parent.tickerId, output)
   }
 }
