a1151c1f5410b890034f91acebc86968fca615b2
Felix Ebert <12857672+felix-ebert@users.noreply.github.com>
2020-11-21 13:42:35 +0100

    Add tailwindcss framework

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './public/**/*.html',
+    './src/**/*.vue'
+  ],
+  theme: {
+    extends: {},
+  },
+  variants: {},
+  plugins: [],
+};
