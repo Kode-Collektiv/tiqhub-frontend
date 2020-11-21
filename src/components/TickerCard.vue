ceeb5e5601a9aba003a3e7527cf3aaadf038443c
j-sattler <mail@sattlerjoshua.de>
2020-11-21 13:29:46 +0100

    Add basic ticker card layout

---
+++ src/components/TickerCard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="card-container">
+    <div class="card-header">
+      <div class="card-logo">
+        <h3>Logo</h3>
+      </div>
+      <div class="card-title">
+        <h1>Title</h1>
+      </div>
+      <div class="card-subtitle">
+        <h3>Subtitle</h3>
+      </div>
+
+    </div>
+    <div class="card-content">
+      <p>Some content that is inside the card</p>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class TickerCard extends Vue {
+  msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.card-container {
+  display: grid;
+  grid-template-rows: 1fr 4fr;
+  height: 300px;
+  border: rgba(100, 100 , 100, 0.8) solid 0.1em;
+  border-radius: 20px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+}
+
+.card-header {
+  border-bottom: rgba(100, 100 , 100, 0.8) solid 0.1em;
+  margin-left: 1em;
+  margin-right: 1em;
+
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+
+}
+
+.card-title {
+
+}
+
+.card-subtitle {
+
+}
+
+.card-logo {
+
+}
+
+.card-content {
+
+}
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TickerCard msg="TickerCard Works!"></TickerCard>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TickerCard from "@/components/TickerCard.vue";
 
 @Options({
   components: {
     HelloWorld,
+    TickerCard
   },
 })
 export default class Home extends Vue {}
